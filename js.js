3edafbe73ffe3710bff136f495c342eedafd42eb
Andrey Filimonov <gipget@mail.ru>
2017-08-19 11:33:04 +0300

    d

---
+++ js.js
@@ -0,0 +1,33 @@
+let button1 = document.querySelector('.button-task-1');
+let button2 = document.querySelector('.button-task-1-2');
+let textarea = document.querySelector('.textar');
+let conteiner = document.querySelector('.conteiner');
+let numberInputs = 1;
+
+button1.addEventListener('click', inputAdd);
+
+function inputAdd(){
+    if(numberInputs<5){
+  conteiner.innerHTML += '<label><input type="text"><button>-</button><br><br></label> '
+  numberInputs +=1;
+}
+}
+
+conteiner.addEventListener('click', inputDelete);
+
+function inputDelete(e){
+  if(e.target.localName == 'button'){
+      e.target.parentNode.remove();
+      numberInputs = numberInputs -1;
+  }
+}
+
+button2.addEventListener('click', inputValue);
+function inputValue(){
+  let inputs = document.querySelectorAll('input');
+  let values = '';
+  for (var index = 0; index < inputs.length; index++) {
+      values += inputs[index].value + '\n';      
+  }
+    textarea.textContent = values;
+}
